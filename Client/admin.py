4b60ddcf11e85ffdaae4b4be72fa50d53a3fe352
Sepehr Rajabi <sepehrrajabi478@gmail.com>
2026-08-15 18:10:49 +0200

    Added admin classes for `Client`

--- Client/admin.py
+++ Client/admin.py
@@ -1,3 +1,53 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Client, ClientContactInfo, ClientType
+
+
+class ClientTypeInAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "title",
+    ]
+    search_fields = [
+        "title",
+    ]
+
+
+admin.site.register(ClientType, ClientTypeInAdmin)
+
+
+class ClientContactInfoInAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "website",
+        "email",
+        "phone",
+        "created_at",
+    ]
+    search_fields = [
+        "website",
+        "email",
+        "phone",
+    ]
+
+
+admin.site.register(ClientContactInfo, ClientContactInfoInAdmin)
+
+
+class ClientInAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "name",
+        "contact_info",
+        "created_at",
+    ]
+    search_fields = [
+        "name",
+        # "client_type__title",
+        # "contact_info__website",
+        # "contact_info__email",
+        # "contact_info__phone",
+    ]
+
+
+admin.site.register(Client, ClientInAdmin)
